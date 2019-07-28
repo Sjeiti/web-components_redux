b5bd07983f828032c10001a360a6474e19df5698
* <github@sjeiti.com>
2019-07-28 09:48:05 +0200

    added experiments

--- src/js/component/Header.js
+++ src/js/component/Header.js
@@ -1,19 +1,31 @@
-import experiments from 'Experiments/src/experiment'
+import experiments from 'Experiments/src/experiment/index.js'
+
 import {component, BaseComponent} from '../Component'
 import {scroll} from '../signal/scroll'
+import {signal} from '../signal'
 import {routeChange} from '../router'
+import {appendChild} from '../utils/html'
 
 component.create('data-header',class extends BaseComponent{
 
   constructor(...args){
     super(...args)
 
+    const stuck = signal()
+
     const header = this._element//document.querySelector('header')
     let lastScrollTop = 0
     let lastHeaderTop = 0
     scroll.add((e,w,h)=>{
       const headerTop = header.getBoundingClientRect().top
-      lastScrollTop!==h&&header.classList.toggle('stuck',h>0&&headerTop===lastHeaderTop)
+      if (lastScrollTop!==h){
+        const isStuck = h>0&&headerTop===lastHeaderTop
+        const wasStuck = header.classList.contains('stuck')
+        if (wasStuck!==isStuck){
+          header.classList.toggle('stuck',isStuck)
+          stuck.dispatch(isStuck)
+        }
+      }
       lastHeaderTop = headerTop
       lastScrollTop = h
       //
@@ -21,14 +33,25 @@ component.create('data-header',class extends BaseComponent{
     })
 
     routeChange.add(location=>{
-      console.log(location.pathname)
+      console.log('pathname',location.pathname)
       const allCurrent = this._element.querySelectorAll(`a[href="${location.pathname}"]`)
       Array.from(allCurrent).forEach(elm=>elm.classList.add('current'))
     })
 
-    for (let name in experiments){
-      console.log(name)
-    }
+    stuck.add(console.log.bind(console,'stuck'))
 
+    if (location.pathname==='/'){
+      for (let name in experiments){
+        console.log(name)
+      }
+      const experimentWrapper = document.createElement('div')
+      experimentWrapper.classList.add('experiment-wrapper')
+      this._element.appendChild(experimentWrapper)
+      const xp = experiments.starzoom.init(experimentWrapper)
+      stuck.add(is=>is?xp.play():xp.pause())
+      for (let prop in xp){
+        console.log('\t',prop)
+      }
+    }
   }
 })
